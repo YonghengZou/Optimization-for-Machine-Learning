348f6a4fcff3db014c94964abf6fde9a5cc2a6a0
Yongheng Zou <38074449+YonghengZou@users.noreply.github.com>
2021-10-20 01:56:51 -0700

    Add files via upload

---
+++ code/f_Beale.m
@@ -0,0 +1,10 @@
+function f = f_Beale(x) 
+x1 = x(1);
+x2 = x(2);
+
+term1 = (1.5 - x1 + x1*x2)^2;
+term2 = (2.25 - x1 + x1*x2^2)^2;
+term3 = (2.625 - x1 + x1*x2^3)^2;
+
+f = term1 + term2 + term3;
+end
